4e3a8e7131e7dd409c94e13758cb89e14ffb7cea
Matthew <redskirt@outlook.com>
2022-01-28 20:46:42 +0800

    _

---
+++ case/src/main/java/com/matthew/interfaces/PersonInterface.java
@@ -0,0 +1,16 @@
+package com.matthew.interfaces;
+
+/**
+ * 接口 interface 
+ * 与
+ * 实现 implements
+ * 
+ * - 只能声明常量和抽象方法
+ * - 
+ * 
+ * @老刘编程 点赞关注私信获取源码
+ *
+ */
+public interface PersonInterface {
+
+}
